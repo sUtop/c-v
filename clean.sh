5e2117ace32d1c4100283ec8207d6fad72ae8b0d
sUtop <Utop@inbox.ru>
2016-03-19 00:32:54 +0300

    Разнесение логики сборки по проектам.

--- clean.sh
+++ clean.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
-find ./ -name CMakeFiles* -exec rm -rf {} ';'
-find ./ -name cmake_install* -exec rm -rf {} ';'
-find ./ -name CMakeCache* -exec rm -rf {} ';'
-find ./ -name .DS_Store -exec rm -rf {} ';'
-find ./ -name Makefile -exec rm -rf {} ';'
-
+find ./ -name "CMakeF*" -exec rm -rf {} ';'
+find ./ -name "CMakeC*" -exec rm -rf {} ';'
+find ./ -name "cmake_install*" -exec rm -rf {} ';'
+find ./ -name ".DS_Store" -exec rm -rf {} ';'
+find ./ -name "Makefile" -exec rm -rf {} ';'
+find ./ -name "moc_*.cpp" -exec rm -rf {} ';'
+find ./ -name "*_automoc*" -exec rm -rf {} ';'
+find ./ -name "bin" -exec rm -rf {} ';'
+rm -rf bin
 # Makefile
